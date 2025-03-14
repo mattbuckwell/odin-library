4626dbca193fa2c2a7f724c85dc4753b8896d540
Matt Buckwell <mattbuckwell@gmail.com>
2025-03-14 13:24:11 +1300

    Add constructor for Book and function to create book and add to the library

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log("Hello from the library");
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+
+addBookToLibrary("Harry Potter", "Billy", "1000", "No");
+addBookToLibrary("Wizard", "Trump", "300", "Yes");
+
+console.log(myLibrary[0].title);
+console.log(myLibrary[1].author);
